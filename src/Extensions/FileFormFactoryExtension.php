a22d96d7d95217e0681e85e5e94a5b7b0263e193
Amol Wankhede <amolsw.nz@gmail.com>
2024-04-15 18:03:30 +1200

    Add Show in search and last indexed field to file details tab

---
+++ src/Extensions/FileFormFactoryExtension.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace SilverStripe\SearchService\Extensions;
+
+use SilverStripe\Assets\Image;
+use SilverStripe\Control\RequestHandler;
+use SilverStripe\Core\Extension;
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\DatetimeField;
+use SilverStripe\Forms\Form;
+use SilverStripe\Forms\FormFactory;
+
+class FileFormFactoryExtension extends Extension
+{
+
+    public function updateForm(
+        Form $form,
+        ?RequestHandler $controller = null,
+        string $name = FormFactory::DEFAULT_NAME,
+        array $context = []
+    ): void {
+        $fields = $form->Fields()->fieldByName('Editor.Details');
+        $file = $context['Record'] ?? null;
+
+        if (!$fields || !$file || $file instanceof Image) {
+            return;
+        }
+
+        $fields->push(
+            CheckboxField::create(
+                'ShowInSearch',
+                _t(
+                    'SilverStripe\\AssetAdmin\\Controller\\AssetAdmin.SHOWINSEARRCH',
+                    'Show in search?'
+                )
+            )
+        );
+
+        $fields->push(
+            DatetimeField::create(
+                'SearchIndexed',
+                _t(
+                    'SilverStripe\\SearchService\\Extensions\\SearchServiceExtension.LastIndexed',
+                    'Last indexed in search'
+                )
+            )
+                ->setReadonly(true)
+        );
+    }
+
+}
